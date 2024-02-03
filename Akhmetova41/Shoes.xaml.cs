b510047c6038cbd2b675c2d3993fa200218752ed
Azalia1Akhmetova <aazalia75@gmail.com>
2024-02-03 23:21:24 +0500

    вывод данных и наличие картинок

---
+++ Akhmetova41/Shoes.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Akhmetova41
+{
+    /// <summary>
+    /// Логика взаимодействия для Shoes.xaml
+    /// </summary>
+    public partial class Shoes : Page
+    {
+        public Shoes()
+        {
+            InitializeComponent();
+            var currentShoes = Akhmetova41Entities.GetContext().Product.ToList();
+            ShoesListView.ItemsSource = currentShoes;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+    }
+}
